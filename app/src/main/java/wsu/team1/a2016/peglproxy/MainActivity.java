ee7256ff3ac7fc07a6452de4f2d01bd6b74d8304
joshuastevenson <stevenson.43@wright.edu>
2017-01-12 11:40:25 -0500

    Initial android studio project

---
+++ app/src/main/java/wsu/team1/a2016/peglproxy/MainActivity.java
@@ -0,0 +1,13 @@
+package wsu.team1.a2016.peglproxy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
